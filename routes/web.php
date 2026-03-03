c6d63782fb5503cb85813eea1f18a57c1b7fdceb
Lyurhndz <dvnrt.25@gmail.com>
2026-03-03 17:15:27 +0700

    Exercise 6 JS3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,5 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/category', [CategoryController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
