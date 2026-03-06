aa837bb23a363a1560d94c101a1d8dd0fa3aa39a
Lyurhndz <dvnrt.25@gmail.com>
2026-03-06 21:48:14 +0700

    Retrieving Single Models Exercise 2 JS4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\UserModel;
 use Illuminate\Support\Facades\DB;
@@ -25,15 +26,27 @@ class UserController extends Controller
         // UserModel::where('username', 'customer-1')->update($data);
 
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_three',
-            'name' => 'Manager 3',
-            'password' => hash::make('12345')
-        ];
-        UserModel::create($data);
+        //JS4
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_three',
+        //     'name' => 'Manager 3',
+        //     'password' => hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // $user = UserModel::find(1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::firstWhere('level_id', 1);
+        // $user = UserModel::findOr(1, ['username', 'name'], function(){
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20, ['username', 'name'], function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
